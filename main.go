4774b58d36dd5eb94dab81ad64aaa28a61a0eb4e
neel <neelxdxd@gmail.com>
2024-02-10 21:42:08 +0530

    Initial commit! Learning GO LANG

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"  // fmt is a package that contains functions for printing formatted output and scanning input
+
+func main(){
+	var name string = "Neel Sheth";
+	fmt.Println(name);
+
+	name = "Neel";
+	fmt.Println(name);
+
+	nametwo := "Sheth";
+	fmt.Println(nametwo);
+}
